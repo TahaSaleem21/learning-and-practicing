062b9093c92539254ad2d209cdb4dc60e5c7fee5
Taha Saleem <tstahasaleem@gmail.com>
2024-03-01 09:47:56 -0800

    styling

---
+++ script.js
@@ -0,0 +1,7 @@
+let button1 = document.querySelector("#button1");
+
+// const...
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
